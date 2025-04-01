690e9fba7306908338e5312c48a3b72fc1d9e5c6
SAETAK <ihjang404@gmail.com>
2025-04-01 15:36:05 +0900

    edit package class

--- system_monitoring/LogToDB.java
+++ system_monitoring/LogToDB.java
@@ -1,5 +1,10 @@
-import java.io.*;
-import java.sql.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
 public class LogToDB {
 	public static void main(String[] args) {
