e86d4d9588d1c1742faa4a67fb09a056c7e25c0d
SAETAK <ihjang404@gmail.com>
2025-04-01 18:52:04 +0900

    edit localhost

--- system_monitoring/src/main/java/inhoan/monitoringToAlarm/InsertQuery.java
+++ system_monitoring/src/main/java/inhoan/monitoringToAlarm/InsertQuery.java
@@ -4,7 +4,7 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class InsertQuery {
-	private final String mariadbUrl = "jdbc:mariadb://localgost:3306/server_monitoring";
+	private final String mariadbUrl = "jdbc:mariadb://localhost:3306/server_monitoring";
 	private final String mariadbUser = "root";
 	private final String mariadbPassword = "57878";
 
