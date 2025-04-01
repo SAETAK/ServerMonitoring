abf78618593c922b66d25a534f714a3d9a076db7
SAETAK <ihjang404@gmail.com>
2025-04-01 15:23:00 +0900

    add MariaDB info

--- system_monitoring/LogToDB.java
+++ system_monitoring/LogToDB.java
@@ -3,6 +3,9 @@ import java.sql.*;
 
 public class LogToDB {
 	public static void main(String[] args) {
-	
+		String mariadbUrl = "jdbc:mariadb://localgost:3306/server_monitoring";
+		String mariadbUser = "root";
+		String mariadbPassword = "57878";
+		String logFile = "/root/server_monitoring/log/sys_monitor.log";
 	}
 }
